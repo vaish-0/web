aa8c1a8649ee77a6f0f91a7cabb8ce0e9d4762d7
root <root@ip-172-31-13-138.ap-south-1.compute.internal>
2022-04-08 05:32:04 +0000

    Web frontend

---
+++ build/precache-manifest.834dcd1d9312a38cef6e03f3dfd89f56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc6da58b1ae49da33abf7a562b9ed83e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1cbe01bc22040925694a",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "364703b002de4cf71add",
+    "url": "/static/js/2.7836c117.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.7836c117.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cbe01bc22040925694a",
+    "url": "/static/js/main.86e14652.chunk.js"
+  },
+  {
+    "revision": "4be130620b223c1c9c69",
+    "url": "/static/js/runtime-main.1cbfc7dc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
